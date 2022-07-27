297753c7725dbb8685221e73186800798d5929bf
Ju, Minji <alswl0w0@gmail.com>
2022-07-27 14:47:43 +0900

    게시물 테스트 데이터 자동생성

--- src/main/java/com/ll/exam/article/ArticleRepository.java
+++ src/main/java/com/ll/exam/article/ArticleRepository.java
@@ -2,6 +2,7 @@ package com.ll.exam.article;
 
 import com.ll.exam.article.dto.ArticleDto;
 
+import java.util.stream.IntStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,9 +13,19 @@ public class ArticleRepository {
     static {
         datum = new ArrayList<>();
         lastId = 0;
+
+        makeTestData();
     }
 
-    public long write(String title, String body) {
+    private static void makeTestData() {
+        IntStream.rangeClosed(1, 10).forEach(id -> {
+            String title = "제목%d".formatted(id);
+            String body = "내용%d".formatted(id);
+            write(title, body);
+        });
+    }
+
+    public static long write(String title, String body) {
         long id = ++lastId;
         ArticleDto newArticleDto = new ArticleDto(id, title, body);
 
@@ -23,11 +34,11 @@ public class ArticleRepository {
         return id;
     }
 
-    public List<ArticleDto> findAll() {
+    public static List<ArticleDto> findAll() {
         return datum;
     }
 
-    public ArticleDto findById(long id) {
+    public static ArticleDto findById(long id) {
         for ( ArticleDto articleDto : datum ) {
             if ( articleDto.getId() == id ) {
                 return articleDto;
